0ca93f57094b55ec4513b1d6a1ee8d7dd3535eaa
devneamulrajib <dev.neamulrajib@gmail.com>
2026-05-03 10:20:42 +0100

    Manual fixes for images on cPanel

--- app/Models/Brokerage.php
+++ app/Models/Brokerage.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Brokerage extends Model
 {
@@ -12,7 +13,25 @@ class Brokerage extends Model
     protected $guarded = [];
 
     protected $casts = [
-        'images' => 'array',
+        'images' => 'array', 
         'amenities' => 'array',
     ];
+
+    /**
+     * Get the URL of the first image.
+     */
+    public function getFirstImageUrlAttribute()
+{
+    if (!$this->images || !is_array($this->images) || count($this->images) === 0) {
+        return null;
+    }
+
+    $path = $this->images[0];
+    
+    // Clean up the path just in case
+    $cleanPath = str_replace('storage/', '', $path);
+    
+    // Hardcode the URL path
+    return 'https://trikonltd.com/storage/' . $cleanPath;
+}
 }
